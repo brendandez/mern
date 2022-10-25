9beac0f066be9287acc94d442564f162cd32cd65
brendandez <brendandezell@gmail.com>
2022-10-25 10:05:41 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <p>*Learn React</p>
+      <p>*Climb Mt.Everest</p>
+      <p>*Run a marathon</p>
+      <p>*Feed the dogs</p>  
+      </div>  
+      
     </div>
   );
 }
